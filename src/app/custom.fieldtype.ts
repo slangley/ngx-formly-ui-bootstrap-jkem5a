367c51d6385d18b51464bb70e8ebd8666c67b09f
slangley <slangley@popcornlabs.com>
2020-05-04 14:00:27 -0400

    1 file modified

--- src/app/custom.fieldtype.ts
+++ src/app/custom.fieldtype.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FieldType } from '@ngx-formly/core';
-import {Subject} from 'rxjs';
+import {Subject, Observable, of} from 'rxjs';
+import {switchMap} from 'rxjs/operators';
 
 @Component({
  selector: 'formly-field-input',
  template: `
-  <p>{{fooz | async}}</p>
+  <p>{{barz | async}}</p>
 
    <input type="input" [formControl]="formControl" [formlyAttributes]="field">
    
@@ -20,16 +21,28 @@ import {Subject} from 'rxjs';
 export class FormlyFieldInput extends FieldType implements OnInit {
 
   private fooz = new Subject<string>();
+  private barz:Observable<string>;
+
 
   ngOnInit() {
     this.fooz.subscribe((val)=>console.log("FOOXZ", val));
 
+    //If you want to use a separate Subject (less than ideal)
     this.form.valueChanges.subscribe( (changes)=> {
         let fooz = changes[this.to.fooz];
         if (fooz) {
           this.fooz.next(fooz);
         }
     });
+
+
+    // Using switchMap and returning different observable (BETTER!!!)
+    this.barz = this.form.valueChanges.pipe(switchMap((val)=> {
+      console.log("V",val);
+      return of(val[this.to.barz])
+    }));
+
+    
   }
 
   virtualKeyboardClick(event){
